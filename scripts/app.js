ebb0364f02029e0de6dc0c6ec07ef08789145c1d
ibakyrdjiev <ibakyrdjiev@gmail.com>
2015-04-18 16:32:06 +0300

    sammy uploaded

--- scripts/app.js
+++ scripts/app.js
@@ -1,25 +1,55 @@
 var app = app || {};
 
-var $button = $('#upload').click(function () {
-    var picRequester = app.pictureRequster.load(app.constants.BASE_URL);
-    var file = $('input')[0].files[0];
-    //todo multiple uploads
-    //todo this must be in the pictureController
-    //todo check fileName
-    if (app.constants.SUPPORTED_FORMATS.indexOf(file.type) !== -1) {
-        picRequester.uploadPicture(file)
-            .then(function (data) {
-                picRequester.createPictureRepo(data)
-                    .then(function (data) {
-                        console.log(data)
-                    }, function (err) {
-                        console.error(err)
-                    }).done();
-            }, function (err) {
-                console.error(err)
-            });
-    }
+(function () {
+
+    app.router = Sammy(function () {
+        var $selector = $('#wrapper');
+
+        this.get('#/', function () {
+            //todo controller
+            console.log('home');
+        });
+
+        this.get('#/hot', function () {
+            //todo controller
+            console.log('hot');
+        });
+
+        this.get('#/top-ten', function () {
+            //todo controller
+            console.log('top 10');
+        });
+
+        this.get('#/signup-login', function () {
+            //todo controller
+            console.log('login');
+        });
+    });
+    app.router.run('#/');
+
+}());
 
 
-    //}
-});
+//var $button = $('#upload').click(function () {
+//    var picRequester = app.pictureRequster.load(app.constants.BASE_URL);
+//    var file = $('input')[0].files[0];
+//    //todo multiple uploads
+//    //todo this must be in the pictureController
+//    //todo check fileName
+//    if (app.constants.SUPPORTED_FORMATS.indexOf(file.type) !== -1) {
+//        picRequester.uploadPicture(file)
+//            .then(function (data) {
+//                picRequester.createPictureRepo(data)
+//                    .then(function (data) {
+//                        console.log(data)
+//                    }, function (err) {
+//                        console.error(err)
+//                    }).done();
+//            }, function (err) {
+//                console.error(err)
+//            });
+//    }
+//
+//
+//    //}
+//});
